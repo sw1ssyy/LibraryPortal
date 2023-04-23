bc54b0c941c218a8bac1c7bee2c3503dce3e380e
sw1ssyy <johnanderson8484@gmail.com>
2023-04-23 21:17:09 +0100

    Third Commit for LibraryPortal
    - Added templates for edit profile, admin view all students

--- src/main/java/com/example/libraryportal/Service/AccountService.java
+++ src/main/java/com/example/libraryportal/Service/AccountService.java
@@ -2,8 +2,12 @@ package com.example.libraryportal.Service;
 
 import com.example.libraryportal.Models.Account;
 import com.example.libraryportal.Models.AccountRepo;
+import com.example.libraryportal.Models.Book;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Component
 public class AccountService {
@@ -15,6 +19,22 @@ public class AccountService {
 
     public Boolean checkAccountExists(String username, String Password){
         return accountRepo.existsAccountByAccountUserNameAndAccountPassword(username,Password);
+    }
+    public List<Account> SearchStudentByName(String name){
+        if(name == null || name.equals("")){
+            return accountRepo.findAll();
+        }
+        return accountRepo.findAllByAccountUserName(name);
+    }
+
+    public void updateAccount(@PathVariable Long id, Account account){
+        Account updatedAccount = accountRepo.findAccountByAccountID(id);
+        if(updatedAccount == null){
+            throw new RuntimeException("Account: '" + account.getAccountUserName() + "' Does not Exist");
+        }
+        updatedAccount.setAccountUserName(account.getAccountUserName());
+        updatedAccount.setAccountPassword(account.getAccountPassword());
+        accountRepo.save(updatedAccount);
     }
 
     public Account findAccountByUsername(String username){
